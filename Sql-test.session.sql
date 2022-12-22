52ff7e4a8a10d843e6c7ce96f2ccbfeb5e99ce56
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-22 15:56:59 +0200

    Restricts

--- Sql-test.session.sql
+++ Sql-test.session.sql
@@ -7,12 +7,15 @@ CREATE TABLE books(
     publisher varchar(256)
 );
 
+DROP TABLE users;
+
 CREATE TABLE users(
-    first_name varchar(256),
-    last_name varchar(256),
-    email varchar(256),
-    birthday date,
-    gender varchar(128)
+    first_name varchar(256) NOT NULL CHECK (first_name != ''),
+    last_name varchar(256) NOT NULL CHECK (last_name != ''),
+    email varchar(256) NOT NULL UNIQUE CHECK (email != ''),
+    birthday date CHECK (birthday < current_date),
+    gender varchar(128) CHECK (gender != ''),
+    CONSTRAINT name_pair UNIQUE(first_name, last_name)
 );
 
 INSERT INTO users VALUES 
@@ -22,3 +25,7 @@ INSERT INTO users VALUES
 ('Iron', 'Man', 'iron@man.cocm', '1960-06-09', 'male'),
 ('Loki', 'Odyn', 'loki@man.cocm', '1940-06-05', 'male'),
 ('Spyder', 'Man', 'spyderyea@man.cocm', '2001-01-05', 'male');
+
+
+
+
