c224fe78beb0ca8091cfa5ec3d95f460b1d9bf5a
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-23 14:55:30 +0200

    ALTER TABLE

--- Sql-test.session.sql
+++ Sql-test.session.sql
@@ -10,18 +10,37 @@ CREATE TABLE books(
 DROP TABLE users;
 
 CREATE TABLE users(
+    id serial PRIMARY KEY,
     first_name varchar(256) NOT NULL CHECK (first_name != ''),
     last_name varchar(256) NOT NULL CHECK (last_name != ''),
     email varchar(256) NOT NULL UNIQUE CHECK (email != ''),
     birthday date CHECK (birthday < current_date),
-    gender varchar(128) CHECK (gender != ''),
-    CONSTRAINT name_pair UNIQUE(first_name, last_name)
+    gender varchar(128) CHECK (gender != '')
 );
 
-INSERT INTO users VALUES 
-('Clark', 'Kent', 'super@man.com', '1990-09-09', 'male');
+ALTER TABLE users 
+ADD COLUMN height numeric(3,2);
 
-INSERT INTO users VALUES 
+ALTER TABLE users 
+ADD CONSTRAINT "to_height_user" CHECK (height < 3.0);
+
+ALTER TABLE users
+DROP CONSTRAINT "to_height_user";
+
+ALTER TABLE users
+DROP CONSTRAINT "weight_not_0";
+
+ALTER TABLE users
+ADD COLUMN weight numeric(5,2) CONSTRAINT "weight_not_0" CHECK (weight > 0);
+
+ALTER TABLE users
+ADD CONSTRAINT "not_before_1990" CHECK (birthday > '1900-01-01');
+
+
+INSERT INTO users (first_name, last_name, email, birthday, gender, height, weight) VALUES 
+('Clark', 'Kent', 'super@man.com', '1980-09-09', 'male', 3.84, 72.81);
+
+INSERT INTO users (first_name, last_name, email, birthday, gender) VALUES 
 ('Iron', 'Man', 'iron@man.cocm', '1960-06-09', 'male'),
 ('Loki', 'Odyn', 'loki@man.cocm', '1940-06-05', 'male'),
 ('Spyder', 'Man', 'spyderyea@man.cocm', '2001-01-05', 'male');
@@ -33,7 +52,7 @@ CREATE TABLE messages(
     body text NOT NULL CHECK (body != ''),
     author varchar(256) NOT NULL,
     created_at timestamp NOT NULL CHECK (created_at <= current_timestamp) DEFAULT current_timestamp,
-    is_read boolean NULL DEFAULT false
+    is_read boolean NOT NULL DEFAULT false
 );
 
 INSERT INTO messages (author, body) VALUES
