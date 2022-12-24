6e7f5340e668fdd457cc6553a5da647d9277b7d4
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-24 16:23:47 +0200

    hometask v1

--- Sql-test.session.sql
+++ Sql-test.session.sql
@@ -48,19 +48,29 @@ INSERT INTO users (first_name, last_name, email, birthday, gender) VALUES
 ('Spyder', 'Man', 'spyderyea@man.cocm', '2001-01-05', 'male');
 
 DROP TABLE messages;
-
 CREATE TABLE messages(
-    id serial NOT PRIMARY KEY,
+    id serial PRIMARY KEY,
     body text NOT NULL CHECK (body != ''),
-    author varchar(256) NOT NULL,
+    author int REFERENCES users(id),
     created_at timestamp NOT NULL CHECK (created_at <= current_timestamp) DEFAULT current_timestamp,
     is_read boolean NOT NULL DEFAULT false
 );
 
-INSERT INTO messages (author, body) VALUES
-('test user', 'dsdfs'),
-('test user', 'dsdfs'),
-('test user2', 'dsdfsdfd');
+DROP TABLE chats;
+CREATE TABLE chats(
+    id serial PRIMARY KEY,
+    user_id int REFERENCES users(id),
+    message_id int REFERENCES messages(id),
+    chat_name varchar(128) NOT NULL CHECK (chat_name != '')
+);
+
+
+DROP TABLE chats_to_users;
+CREATE TABLE chats_to_users(
+    user_id int REFERENCES users(id),
+    chats_id int REFERENCES chats(id),
+    PRIMARY KEY (user_id, chats_id)
+);
 
 
 
@@ -97,3 +107,8 @@ CREATE TABLE orders_to_products(
     quantity int CHECK (quantity > 0),
     PRIMARY KEY (product_id, order_id)
 );
+
+INSERT INTO orders_to_products(product_id, order_id, quantity) VALUES
+(1, 7, 1),
+(2, 8, 1),
+(3, 9, 1);
