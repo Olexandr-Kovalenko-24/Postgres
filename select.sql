70a60808192e82cca192201c858439403bef5891
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-29 16:49:40 +0200

    UPDATE, DELETE WHERE

--- select.sql
+++ select.sql
@@ -21,3 +21,48 @@ SELECT email FROM users
 WHERE is_subscribed;
 
 
+SELECT * FROM users
+WHERE first_name = 'John';
+
+SELECT * FROM users
+WHERE first_name IN ('John', 'Clark');
+
+
+SELECT * FROM users
+WHERE id IN (1, 10, 2500);
+
+
+SELECT * FROM users
+WHERE id > 2500 AND id < 3000;
+
+
+SELECT * FROM users
+WHERE id BETWEEN 2500 AND 3000;
+
+
+SELECT * FROM users
+WHERE first_name LIKE 'K%';
+
+
+SELECT * FROM users
+WHERE first_name LIKE '_____';
+
+
+SELECT * FROM users
+WHERE first_name LIKE '%a';
+
+
+UPDATE users
+SET weight = 60
+WHERE id BETWEEN 2500 AND 2550;
+
+
+UPDATE users
+SET weight = 70
+WHERE id % 5 = 0 RETURNING *;
+
+DELETE FROM users
+WHERE id = 2222
+RETURNING *;
+
+
