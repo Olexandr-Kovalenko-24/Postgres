61639d1cc729cd95e61693ead8c57426e8a00913
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-12-29 15:45:39 +0200

    generation

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const {User} = require('./models/index');
+const {client, User, Phone, Order} = require('./models/index');
 const {getUsers} = require('./api/index');
+const {generatePhones} = require('./utils/generate');
 
 
 async function start(){
     await client.connect();
     const usersArray = await getUsers();
-    const {rows} = await User.bulkCreate(usersArray);
+    // const {rows} = await User.bulkCreate(usersArray);
+    const {rows: users} = await User.findAll();
+    // const phones = await Phone.bulkCreate(generatePhones(100));
+    const {rows: phones} = await Phone.findAll();
+    const orders = await Order.bulkCreate(users, phones);
     await client.end();
 }
 
